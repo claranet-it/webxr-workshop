e3c0aac89f4ae93d997622311be87ad6fb570385
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2021-02-05 13:41:33 +0100

    Added eventstorming example

--- src/10-Data/components.js
+++ src/10-Data/components.js
@@ -113,17 +113,59 @@ AFRAME.registerComponent('rotation-receiver', {
           position
         } = detail
 
-        component.el.setAttribute('position', {
-          x: -position.x,
-          y: position.y,
-          z: -position.z
-        })
+        window.requestAnimationFrame(() => {
+          component.el.setAttribute('position', {
+            x: -position.x,
+            y: position.y,
+            z: -position.z
+          })
 
-        component.el.setAttribute('rotation', {
-          x: -90 + radToDeg(rotation.x),
-          y: radToDeg(rotation.y),
-          z: 180 + radToDeg(rotation.z)
+          component.el.setAttribute('rotation', {
+            x: -90 + radToDeg(rotation.x),
+            y: radToDeg(rotation.y),
+            z: 180 + radToDeg(rotation.z)
+          })
         })
       })
+  }
+})
+
+const extendDeep = AFRAME.utils.extendDeep
+const meshMixin = AFRAME.primitives.getMeshMixin()
+
+AFRAME.registerPrimitive('a-post-it', extendDeep({}, meshMixin, {
+  defaultComponents: {
+    geometry: {
+      primitive: 'plane',
+      height: 0.076,
+      width: 0.076
+    },
+    material: {
+      shader: 'flat',
+      side: 'double'
+    },
+    rotation: {
+      x: 0,
+      y: 90,
+      z: 0
+    }
+  },
+  mappings: {
+    color: 'material.color'
+  }
+}))
+
+const TYPES = {
+  external: '#FC67B9',
+  command: '#3799FE',
+  aggregate: '#FBD32D',
+  event: '#FA992A'
+}
+
+AFRAME.registerComponent('type', {
+  schema: { type: 'string' },
+  init: function () {
+    const color = TYPES[this.data] || TYPES.event
+    this.el.setAttribute('color', color)
   }
 })
